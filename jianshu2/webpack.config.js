dde370915317c5dac639a021f9f11616166e719e
komolei <944545149@qq.com>
2017-07-22 00:06:25 +0800

    add postcss

--- jianshu2/webpack.config.js
+++ jianshu2/webpack.config.js
@@ -33,71 +33,73 @@ module.exports = {
         }),
         new webpack.ProvidePlugin({
             $: 'jquery'
-        })
+        }),
+        require('autoprefixer')
     ],
     module: {
         rules: [{
-                test: /\.js[x]?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                })
-            },
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', {
-                        loader: 'less-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }]
-                })
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }]
-                })
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|svg|swf)$/,
-                use: {
-                    loader: 'file-loader',
+            test: /\.js[x]?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: [{
+                    loader: 'css-loader',
+                }, {
+                    loader: 'postcss-loader',
+                }]
+
+            }
+            )
+        },
+        {
+            test: /\.less$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', {
+                    loader: 'less-loader',
                     options: {
-                        name: '[name]_[sha512:hash:base64:7].[ext]'
+                        sourceMap: true
                     }
-                }
-            },
-            {
-                test: /\.html/,
-                use: {
-                    loader: "html-loader",
+                }]
+            })
+        },
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', {
+                    loader: 'sass-loader',
                     options: {
-                        minimize: false,
-                        attrs: false
+                        sourceMap: true
                     }
+                }]
+            })
+        },
+        {
+            test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|svg|swf)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: '[name]_[sha512:hash:base64:7].[ext]'
                 }
             }
+        },
+        {
+            test: /\.html/,
+            use: {
+                loader: "html-loader",
+                options: {
+                    minimize: false,
+                    attrs: false
+                }
+            }
+        }
         ]
     }
 };
