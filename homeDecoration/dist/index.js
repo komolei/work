007366da521f40dec38348215c93782f95de80c1
komolei <944545149@qq.com>
2017-07-25 10:52:01 +0800

    finish

--- homeDecoration/dist/index.js
+++ homeDecoration/dist/index.js
@@ -8215,14 +8215,14 @@ fixboxP.on("click", function () {
 // console.log(up);
 
 
-// 工装 有问题
+// 工装 有问题 等等有空去优化一下
 var first = $(".gongzhuang-content .first");
 var second = $(".gongzhuang-content .second");
 var third = $(".gongzhuang-content .third");
 console.log(first);
 first.on("mouseover", function () {
     console.log(1);
-    $(".gongzhuang-content .second div").css({
+    $(".gongzhuang-content .second div").eq(0).css({
         opacity: .3,
         marginTop: "15px"
 
@@ -8233,13 +8233,103 @@ first.on("mouseover", function () {
 
     });
     $(".gongzhuang-content .first img").animate({
-        height: "630px"
+        height: "660px"
         // width: "125%"
 
     });
-    $(".gongzhuang-content .first a>div").css({
+    $(".gongzhuang-content .first div").eq(0).css({
         opacity: 1,
+        marginTop: "-30px"
+
+    });
+});
+first.on("mouseleave", function () {
+    $(".gongzhuang-content .first div").eq(0).css({
+        opacity: .3,
         marginTop: "15px"
+
+    });
+    $(".gongzhuang-content .first img").animate({
+        height: "600px",
+        width: "100%"
+
+    });
+    $(".gongzhuang-content .second img").animate({
+        height: "660px"
+        // width: "125%"
+
+    });
+    $(".gongzhuang-content .second div").eq(0).css({
+        opacity: 1,
+        marginTop: "-15px"
+        // height: "630px"
+
+    });
+});
+second.on("mouseover", function () {
+    $(".gongzhuang-content .second div").eq(0).css({
+        opacity: 1,
+        marginTop: "-15px"
+
+    });
+    $(".gongzhuang-content .second img").animate({
+        height: "660px",
+        width: "100%"
+
+    });
+    $(".gongzhuang-content .third img").animate({
+        height: "600px"
+        // width: "125%"
+
+    });
+    $(".gongzhuang-content .third div").eq(0).css({
+        opacity: .3,
+        marginTop: "0px"
+
+    });
+});
+third.on("mouseover", function () {
+    $(".gongzhuang-content .second div").eq(0).css({
+        opacity: .3,
+        marginTop: "15px"
+
+    });
+    $(".gongzhuang-content .second img").animate({
+        height: "600px",
+        width: "100%"
+
+    });
+    $(".gongzhuang-content .third img").animate({
+        height: "660px"
+        // width: "125%"
+
+    });
+    $(".gongzhuang-content .third div").eq(0).css({
+        opacity: 1,
+        marginTop: "-30px"
+
+    });
+});
+third.on("mouseleave", function () {
+    $(".gongzhuang-content .third div").eq(0).css({
+        opacity: .3,
+        marginTop: "0px"
+
+    });
+    $(".gongzhuang-content .third img").animate({
+        height: "600px",
+        width: "100%"
+
+    });
+    $(".gongzhuang-content .second img").animate({
+        height: "660px"
+        // width: "125%"
+
+    });
+    $(".gongzhuang-content .second div").eq(0).css({
+        opacity: 1,
+        marginTop: "-15px"
+        // height: "630px"
 
     });
 });
